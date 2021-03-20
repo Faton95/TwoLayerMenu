55ca4633cfe96accd2583cb42e7b1035a48d61c4
User <user@Users-MBP.mshome.net>
2021-03-20 15:07:06 +0500

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useContext} from 'react'
+import { useAccordionToggle } from 'react-bootstrap/AccordionToggle';
+import {Accordion, AccordionContext} from "react-bootstrap";
+import {menuList} from './menu'
+import './style.scss'
+
+function ContextAwareToggle({ children, eventKey, callback }) {
+    const currentEventKey = useContext(AccordionContext);
+
+    const decoratedOnClick = useAccordionToggle(
+        eventKey,
+        () => callback && callback(eventKey),
+    );
+
+    const isCurrentEventKey = currentEventKey === eventKey;
+
+    return (
+        <button
+            type="button"
+            style={{ backgroundColor: isCurrentEventKey ? '#5B7ED7' : 'lavender' }}
+            onClick={decoratedOnClick}
+        >
+            {children}
+        </button>
+    );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Accordion defaultActiveKey="0">
+            <div>
+                <ContextAwareToggle className="menu" variant="link" eventKey="0">
+                    Click me!
+                </ContextAwareToggle>
+                <Accordion.Collapse eventKey="0">
+                    <Accordion style={{marginLeft: 30}} defaultActiveKey="0">
+                        {menuList.map((menu, index) => {
+                            return (
+                                <div>
+                                    <ContextAwareToggle variant="link" eventKey={index+1}>
+                                        <span>{menu.parentStateName}</span> - <span>{menu.count}</span>
+                                    </ContextAwareToggle>
+                                    <Accordion.Collapse eventKey={index+1} style={{marginLeft: 30}}>
+                                        <ul>
+                                            {menu.items.map((subMenu) => {
+                                                return (
+                                                    <li>
+                                                        <span>{subMenu.stateName}</span> - <span>{subMenu.count}</span>
+                                                    </li>
+                                                )
+                                            })}
+                                        </ul>
+                                    </Accordion.Collapse>
+                                </div>
+                            )
+                        })}
+                    </Accordion>
+                </Accordion.Collapse>
+            </div>
+            <div>
+                <ContextAwareToggle variant="link" eventKey="1">
+                    Click me!
+                </ContextAwareToggle>
+                <Accordion.Collapse eventKey="1">
+                    <div>Hello! I'm another body</div>
+                </Accordion.Collapse>
+            </div>
+        </Accordion>
     </div>
   );
 }
---
+++ src/menu.js
@@ -0,0 +1,135 @@
+export const menuList = [
+    {
+        "parentStateId": 1,
+        "parentStateName": "Солиқ аудитига номзодлар",
+        "count": 247,
+        "items": [
+            {
+                "stateId": 100,
+                "stateName": "Солиқ аудитига номзодлар",
+                "count": 247
+            }
+        ]
+    },
+    {
+        "parentStateId": 2,
+        "parentStateName": "Солиқ аудитига тайёргарлик",
+        "count": 0,
+        "items": [
+            {
+                "stateId": 200,
+                "stateName": "Солиқ тўловчи фаолиятини ўрганиш",
+                "count": 0
+            },
+            {
+                "stateId": 205,
+                "stateName": "ДСҚга хат жўнатиш",
+                "count": 0
+            },
+            {
+                "stateId": 210,
+                "stateName": "Солиқ аудити ўтказиш тўғрисида хабарнома",
+                "count": 0
+            },
+            {
+                "stateId": 215,
+                "stateName": "Омбудсманга хат жўнатиш",
+                "count": 0
+            },
+            {
+                "stateId": 220,
+                "stateName": "Солиқ аудитини ўтказиш дастури",
+                "count": 0
+            },
+            {
+                "stateId": 230,
+                "stateName": "Солиқ аудитини ўтказиш буйруғи",
+                "count": 0
+            }
+        ]
+    },
+    {
+        "parentStateId": 3,
+        "parentStateName": "Қўшимча назорат тадбири",
+        "count": 0,
+        "items": [
+            {
+                "stateId": 300,
+                "stateName": "Ҳудудларни кўздан кечириш",
+                "count": 0
+            },
+            {
+                "stateId": 310,
+                "stateName": "Ҳужжатларни талаб қилиш",
+                "count": 0
+            },
+            {
+                "stateId": 320,
+                "stateName": "Учинчи шахслардан ҳужжатлар (ахборот) талаб қилиш",
+                "count": 0
+            },
+            {
+                "stateId": 330,
+                "stateName": "Савдо қоидаларига риоя этилишини текшириш",
+                "count": 0
+            },
+            {
+                "stateId": 340,
+                "stateName": "Қимматликларни инвентаризациядан ўтказиш",
+                "count": 0
+            },
+            {
+                "stateId": 350,
+                "stateName": "Гувоҳлар (холислар) кўрсатмаси",
+                "count": 0
+            },
+            {
+                "stateId": 360,
+                "stateName": "Ҳужжатларни (буюмларни олиб қўйиш)",
+                "count": 0
+            },
+            {
+                "stateId": 370,
+                "stateName": "Экспертиза тайинлаш",
+                "count": 0
+            },
+            {
+                "stateId": 380,
+                "stateName": "Солиқ аудити далолатномасини тузиш",
+                "count": 0
+            },
+            {
+                "stateId": 390,
+                "stateName": "Маъмурий ҳуқуқбузарлик тўғрисида баённома тузиш",
+                "count": 0
+            },
+            {
+                "stateId": 500,
+                "stateName": "Буйруқ расмийлаштириш",
+                "count": 0
+            },
+            {
+                "stateId": 510,
+                "stateName": "Ҳужжатларни талаб қилиш",
+                "count": 0
+            }
+        ]
+    },
+    {
+        "parentStateId": 4,
+        "parentStateName": "Солиқ аудити натижаларини кўриб чиқиш",
+        "count": 0,
+        "items": [
+            {
+                "stateId": 400,
+                "stateName": "Аудит материалларини кўриб чиқиш тўғрисида хабарнома",
+                "count": 0
+            },
+            {
+                "stateId": 410,
+                "stateName": "Солиқ тўловчиларнинг ёзма эътирозлари",
+                "count": 0
+            }
+        ]
+    }
+]
